6f9d51c27edeeeb9f3d83573975fe69d9b5e30c2
Aurea <aurealet20146@gmail.com>
2026-08-31 11:57:41 -0300

    chore: ignore eslint and typescript errors during vercel build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.107'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   /* config options here */
 };
 
